823017cf966cd3d2ccf0cae52f09e741121f30f9
Aniket Mahakalkar <140543955+aniket-mahakalkar@users.noreply.github.com>
2026-06-10 11:39:57 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.go
@@ -0,0 +1,41 @@
+func numDecodings(s string) int {
+
+
+	if len(s) == 0 || s[0] == '0' {
+
+		return 0
+	}
+
+	n := len(s)
+	dp := make([]int, n+1)
+
+
+	dp[0] = 1
+	dp[1] = 1
+
+
+
+	for i := 2; i <= n; i++{
+
+		if s[i-1] != '0'{
+
+			dp[i] += dp[i-1]
+		}
+
+
+		twoDigit := int(s[i-2] - '0')*10 + int(s[i-1]-'0') 
+
+
+
+		if twoDigit >= 10 && twoDigit <= 26 {
+
+			dp[i] += dp[i-2]
+		}
+
+	}
+
+	return dp[n]
+
+
+    
+}
